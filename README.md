3de750325f739a3344ed40b9c90d1ee6f2546fdc
Stefan Bethge <stefan+github@lanpartei.de>
2015-01-30 17:42:20 +0100

    update to express 4, some clean up and updated README

--- README.md
+++ README.md
@@ -1,31 +1,33 @@
 Hello-OBP-OAuth1.0a-Node
 ========================
 
-This is a very basic app to demonstrate the integration of the OpenBankProject with OAuth1.0-Authentication into a NodeJS application.
+This is a very basic app to demonstrate the integration of the OpenBankProject with OAuth1.0-Authentication
+into a NodeJS application.
 
 ## SETUP
 
-Get consumer key / secret:  
-register your client at  
-https://apisandbox.openbankproject.com/consumer-registration  
-and use the credentials as _openbankConsumerKey/_openbankConsumerSecret in oauth.js  
+Get consumer key / secret:
+register your client at https://apisandbox.openbankproject.com/consumer-registration
+and use the credentials as _openbankConsumerKey/_openbankConsumerSecret in oauth.js
 
-Install Dependencies:  
-npm install express@3.5.2  
-npm install oauth  
+Install Dependencies:
+npm install
 
-Start Server:  
-node oauth.js  
+Start Server:
+node oauth.js
 
-Navigate to the page:  
-Local host: http://127.0.0.1:8080  
+Navigate to the page:
+http://127.0.0.1:8080
+(not http://localhost:8080 in this example as the session won't be set)
 
 You can log in as a test user, using the following credentials:
 
-username: joe.bloggs@example.com  
+username: joe.bloggs@example.com
 password: qwerty
 
 ## REFERENCES
 
-[http://moonlitscript.com/post.cfm/how-to-use-oauth-and-twitter-in-your-node-js-expressjs-app/](http://moonlitscript.com/post.cfm/how-to-use-oauth-and-twitter-in-your-node-js-expressjs-app/)  
+[https://github.com/ciaranj/node-oauth](https://github.com/ciaranj/node-oauth)
+[http://webapplog.com/node-js-oauth1-0-and-oauth2-0-twitter-api-v1-1-examples/](http://webapplog.com/node-js-oauth1-0-and-oauth2-0-twitter-api-v1-1-examples/)
+[http://moonlitscript.com/post.cfm/how-to-use-oauth-and-twitter-in-your-node-js-expressjs-app/](http://moonlitscript.com/post.cfm/how-to-use-oauth-and-twitter-in-your-node-js-expressjs-app/)
 [https://gist.github.com/joshj/1933640](https://gist.github.com/joshj/1933640)
--- oauth.js
+++ oauth.js
@@ -15,84 +15,98 @@
 // limitations under the License.
 
 // Open Bank Project (http://www.openbankproject.com)
-// Copyright 2011,2014 TESOBE / Music Pictures Ltd
+// Copyright 2011-2015 TESOBE / Music Pictures Ltd.
 
 // This product includes software developed at
 // TESOBE (http://www.tesobe.com/)
 // by
 // TESOBE: contact AT tesobe DOT com
 // Nina Gänsdorfer: nina AT tesobe DOT com
+// Everett Sochowski: everett AT tesobe DOT com
+// Stefan Bethge: stefan AT tesobe DOT com
 
 var express = require('express');
+var session = require('express-session')
 var util = require('util');
 var oauth = require('oauth');
- 
-var app = express();
- 
 
-// To get the values for the following fields, please register your client here: 
+var app = express();
+
+// To get the values for the following fields, please register your client here:
 // https://apisandbox.openbankproject.com/consumer-registration
 var _openbankConsumerKey = "yourOpenBankConsumerKey";
 var _openbankConsumerSecret = "yourOpenBankConsumerSecret";
- 
+
+var base_url = 'https://apisandbox.openbankproject.com';
 var consumer = new oauth.OAuth(
-  'https://apisandbox.openbankproject.com/oauth/initiate',
-  'https://apisandbox.openbankproject.com/oauth/token',
+  base_url + '/oauth/initiate',
+  base_url + '/oauth/token',
   _openbankConsumerKey,
   _openbankConsumerSecret,
-  '1.0', 
-  'http://127.0.0.1:8080/callback', 
+  '1.0',                             //rfc oauth 1.0, includes 1.0a
+  'http://127.0.0.1:8080/callback',
   'HMAC-SHA1');
- 
-app.configure(function(){
-	app.use(express.cookieParser());
-	app.use(express.session({ secret: "very secret" }));
-});
+
+var cookieParser = require('cookie-parser');
+app.use(session({
+  secret: "very secret",
+  resave: false,
+  saveUninitialized: true
+}));
 
 
 app.get('/connect', function(req, res){
-	consumer.getOAuthRequestToken(function(error, oauthToken, oauthTokenSecret, results){
-		if (error) {
-			res.send("Error getting OAuth request token : " + util.inspect(error), 500);
-		} else {
-			req.session.oauthRequestToken = oauthToken;
-			req.session.oauthRequestTokenSecret = oauthTokenSecret;
-			res.redirect("https://apisandbox.openbankproject.com/oauth/authorize?oauth_token="+req.session.oauthRequestToken);
-		}
-	});
+  consumer.getOAuthRequestToken(function(error, oauthToken, oauthTokenSecret, results){
+    if (error) {
+      res.status(500).send("Error getting OAuth request token : " + util.inspect(error));
+    } else {
+      req.session.oauthRequestToken = oauthToken;
+      req.session.oauthRequestTokenSecret = oauthTokenSecret;
+      res.redirect(base_url + "/oauth/authorize?oauth_token="+req.session.oauthRequestToken);
+    }
+  });
 });
 
 
 app.get('/callback', function(req, res){
-	consumer.getOAuthAccessToken(req.session.oauthRequestToken, req.session.oauthRequestTokenSecret, req.query.oauth_verifier, function(error, oauthAccessToken, oauthAccessTokenSecret, results) {
-		if (error) {
-			res.send("Error getting OAuth access token : " + util.inspect(error) + "["+oauthAccessToken+"]"+ "["+oauthAccessTokenSecret+"]"+ "["+util.inspect(results)+"]", 500);
-		} else {
-			req.session.oauthAccessToken = oauthAccessToken;
-			req.session.oauthAccessTokenSecret = oauthAccessTokenSecret; 
-			res.redirect('/signed_in');
-		}
-	});
+  consumer.getOAuthAccessToken(
+    req.session.oauthRequestToken,
+    req.session.oauthRequestTokenSecret,
+    req.query.oauth_verifier,
+    function(error, oauthAccessToken, oauthAccessTokenSecret, result) {
+      if (error) {
+        //oauthAccessToken, -Secret and result are now undefined
+        res.status(500).send("Error getting OAuth access token : " + util.inspect(error));
+      } else {
+        //error is now undefined
+        req.session.oauthAccessToken = oauthAccessToken;
+        req.session.oauthAccessTokenSecret = oauthAccessTokenSecret;
+        res.redirect('/signed_in');
+      }
+    }
+  );
 });
 
 
 app.get('/signed_in', function(req, res){
-	res.send('Signing in by OAuth worked. Now you can do API calls on private data like this one: <br><a href="/getBanks">Get private banks</a>')
+  res.status(200).send('Signing in by OAuth worked. Now you can do API calls on private data like this one: <br><a href="/getBanks">Get private banks</a>')
 });
 
+
 app.get('/getBanks', function(req, res){
-	consumer.get("https://apisandbox.openbankproject.com/obp/v1.2.1/banks/rbs/accounts/private",
-	req.session.oauthAccessToken,
-	req.session.oauthAccessTokenSecret,
-	function (error, data, response) {
-	    var parsedData = JSON.parse(data);
-	    res.send(parsedData)
-	});
+  consumer.get("https://apisandbox.openbankproject.com/obp/v1.2.1/banks/rbs/accounts/private",
+  req.session.oauthAccessToken,
+  req.session.oauthAccessTokenSecret,
+  function (error, data, response) {
+      var parsedData = JSON.parse(data);
+      res.status(200).send(parsedData)
+  });
 });
 
 
 app.get('*', function(req, res){
-	res.redirect('/connect');
+  res.redirect('/connect');
 });
- 
+
 app.listen(8080);
+
