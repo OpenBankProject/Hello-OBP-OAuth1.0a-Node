b8b3b1b2bb97c004f43ecfdd276d4e50be57b642
mark-tesobe <mark.silva@tesobe.com>
2023-02-14 16:42:41 +0800

    WIP: vue SSR and bootstrap

--- src/app.ts
+++ src/app.ts
@@ -25,18 +25,27 @@ app.get("/", async (_, response) => {
   const html = await renderToString(vueApp);
   response.send(`
       <!DOCTYPE html>
-      <html lang="en">
+      <html lang>
       <html>
         <head>
-          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-          <title>Vue SSR Example</title>
+          <title>Vue SSR</title>
           <link rel="stylesheet" href="public/style.css">
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"/>
+          <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-vue/2.23.1/bootstrap-vue.min.css"/>
           <script type="importmap">
             {
               "imports": {
-                "vue": "https://unpkg.com/vue@3/dist/vue.esm-browser.js"
+                "vue":               "https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.41/vue.esm-browser.prod.js",
+                "vue-router":        "https://cdnjs.cloudflare.com/ajax/libs/vue-router/4.1.5/vue-router.esm-browser.min.js",
+                "@vue/devtools-api": "https://unpkg.com/@vue/devtools-api@6.4.5/lib/esm/index.js",
+                "bootstrap-vue":     "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-vue/2.23.1/bootstrap-vue.esm.min.js"
               }
             }
+          </script>
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+            crossorigin="anonymous">
           </script>
           <script type="module" src="public/app.js"></script>
         </head>
